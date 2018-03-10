81f9943c8cb18aaad422395111f30d01808d2d70
aileaile <541114700@qq.com>
2018-03-10 13:19:12 +0800

    “坐下”功能代码复原

--- src/main/java/com/LL/Triangle/web/LobbyController.java
+++ src/main/java/com/LL/Triangle/web/LobbyController.java
@@ -1,10 +1,13 @@
 package com.LL.Triangle.web;
 
+import com.LL.Triangle.pojo.User;
+import com.LL.Triangle.utils.JsonUtil;
+import com.LL.Triangle.webSocket.LobbyWebSocket;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/lobby")
@@ -12,12 +15,17 @@ public class LobbyController {
 
     @RequestMapping("/sit")
     @ResponseBody
-    public String sit(String mydata,HttpServletRequest request){
-        String s = request.getParameter("mydata");
-        System.out.println(s);
-        System.out.println(mydata);
-        return "12344";
+    public String sit(String data,HttpSession session)  {
+        int table = Integer.parseInt(JsonUtil.getNode(data, "table"));
+        int seat = Integer.parseInt(JsonUtil.getNode(data, "seat"));
+        User user = new User(session.getId(), JsonUtil.getNode(data, "userName"));
+        if (table == 1)
+        {
+            boolean flg = TableOne.sit(seat, user);
+            if (flg) {
+                LobbyWebSocket.broadcastMsg(TableOne.getAll());
+            }
+        }
+        return "";
     }
-
-
 }
